f32cd41dff5ba1f3d7ada8904efdadec3a64392d
Johannes Rudolph <johannes.rudolph@gmail.com>
2019-07-31 17:39:41 +0200

    First stab at conversion to stage
    Some tests are know to be failing

--- akka-http-core/src/main/scala/akka/http/impl/engine/client/PoolInterfaceActor.scala
+++ akka-http-core/src/main/scala/akka/http/impl/engine/client/PoolInterfaceActor.scala
@@ -4,7 +4,9 @@
 
 package akka.http.impl.engine.client
 
+import akka.Done
 import akka.actor._
+import akka.pattern.ask
 import akka.event.{ LogSource, Logging, LoggingAdapter }
 import akka.http.impl.engine.client.PoolFlow._
 import akka.http.impl.engine.client.pool.NewHostConnectionPool
@@ -13,29 +15,140 @@ import akka.http.scaladsl.model._
 import akka.http.scaladsl.Http
 import akka.http.scaladsl.settings.PoolImplementation
 import akka.macros.LogHelper
+import akka.stream.ActorMaterializer
+import akka.stream.Attributes
+import akka.stream.FlowShape
+import akka.stream.Inlet
+import akka.stream.Outlet
+import akka.stream.OverflowStrategy
 import akka.stream.actor.ActorPublisherMessage._
 import akka.stream.actor.ActorSubscriberMessage._
 import akka.stream.actor.{ ActorPublisher, ActorSubscriber, ZeroRequestStrategy }
 import akka.stream.impl.{ Buffer, SeqActorName }
+import akka.stream.scaladsl.Flow
+import akka.stream.scaladsl.Keep
 import akka.stream.scaladsl.{ Sink, Source }
+import akka.stream.stage.GraphStageLogic
+import akka.stream.stage.GraphStageWithMaterializedValue
+import akka.stream.stage.InHandler
+import akka.stream.stage.OutHandler
 import akka.stream.{ BufferOverflowException, Materializer }
+import akka.util.Timeout
 
 import scala.annotation.tailrec
+import scala.concurrent.ExecutionContext
+import scala.concurrent.Future
 import scala.concurrent.Promise
-import scala.concurrent.duration.FiniteDuration
+import scala.concurrent.duration._
 
 private[http] trait PoolInterface {
   def request(request: HttpRequest, responsePromise: Promise[HttpResponse]): Unit
-  def shutdown(): Unit
+  def shutdown()(implicit ec: ExecutionContext): Future[Done]
 }
 
 private object PoolInterfaceActor {
-  def create(gateway: PoolGateway, parent: ActorRefFactory)(implicit fm: Materializer): PoolInterface = new PoolInterface {
-    val ref = parent.actorOf(props(gateway))
-    override def request(request: HttpRequest, responsePromise: Promise[HttpResponse]): Unit =
+  def createOld(gateway: PoolGateway, parent: ActorRefFactory)(implicit fm: Materializer): PoolInterface = new PoolInterface {
+    val ref = parent.actorOf(props(gateway), name.next())
+    def request(request: HttpRequest, responsePromise: Promise[HttpResponse]): Unit =
       ref ! PoolRequest(request, responsePromise)
-    override def shutdown(): Unit =
-      ref ! Shutdown
+    def shutdown()(implicit ec: ExecutionContext): Future[Done] =
+      ref.ask(Shutdown)(Timeout(30.seconds)).mapTo[Done] // FIXME
+  }
+  def create(gateway: PoolGateway, parent: ActorRefFactory)(implicit fm: Materializer): PoolInterface = {
+    val GatewayLogSource: LogSource[PoolGateway] =
+      new LogSource[PoolGateway] {
+        def genString(gateway: PoolGateway): String = {
+          val scheme = if (gateway.hcps.setup.connectionContext.isSecure) "https" else "http"
+          s"Pool(${gateway.gatewayId.name}->$scheme://${gateway.hcps.host}:${gateway.hcps.port})"
+        }
+        override def genString(gateway: PoolGateway, system: ActorSystem): String = s"${system.name}/${genString(gateway)}"
+
+        override def getClazz(t: PoolGateway): Class[_] = classOf[PoolGateway]
+      }
+
+    //import context.system
+    import gateway.hcps
+    import hcps._
+    import setup.{ connectionContext, settings }
+    implicit val system = fm.asInstanceOf[ActorMaterializer].system
+    val log: LoggingAdapter = Logging(system, gateway)(GatewayLogSource)
+
+    log.debug("Creating pool.")
+
+    val connectionFlow =
+      Http().outgoingConnectionUsingContext(host, port, connectionContext, settings.connectionSettings, setup.log)
+
+    val poolFlow =
+      settings.poolImplementation match {
+        case PoolImplementation.Legacy =>
+          log.warning("Legacy pool implementation is deprecated and will be removed in the future. " +
+            "Please start using `akka.http.host-connection-pool.pool-implementation = new`, instead.")
+          PoolFlow(connectionFlow, settings, log).named("PoolFlow")
+        case PoolImplementation.New => NewHostConnectionPool(connectionFlow, settings, log).named("PoolFlow")
+      }
+
+    Flow.fromGraph(new PoolInterfaceStage(gateway, log))
+      .buffer((settings.maxOpenRequests - settings.maxConnections) min 0, OverflowStrategy.backpressure) // FIXME: is that accurate enough?
+      .join(poolFlow)
+      .run()
+  }
+
+  class PoolInterfaceStage(gateway: PoolGateway, log: LoggingAdapter) extends GraphStageWithMaterializedValue[FlowShape[ResponseContext, RequestContext], PoolInterface] {
+    private val requestOut = Outlet[RequestContext]("PoolInterface.requestOut")
+    private val responseIn = Inlet[ResponseContext]("PoolInterface.responseIn")
+    override def shape = FlowShape(responseIn, requestOut)
+
+    private[this] val PoolOverflowException = new BufferOverflowException( // stack trace cannot be prevented here because `BufferOverflowException` is final
+      s"Exceeded configured max-open-requests value of [${gateway.hcps.setup.settings.maxOpenRequests}]. This means that the request queue of this pool (${gateway.hcps}) " +
+        s"has completely filled up because the pool currently does not process requests fast enough to handle the incoming request load. " +
+        "Please retry the request later. See http://doc.akka.io/docs/akka-http/current/scala/http/client-side/pool-overflow.html for " +
+        "more information.")
+
+    override def createLogicAndMaterializedValue(inheritedAttributes: Attributes): (GraphStageLogic, PoolInterface) = {
+      object Logic extends GraphStageLogic(shape) with PoolInterface with InHandler with OutHandler {
+        import gateway.hcps
+
+        setHandlers(responseIn, requestOut, this)
+
+        override def preStart(): Unit = pull(responseIn)
+
+        override def onPush(): Unit = {
+          val ResponseContext(rc, response) = grab(responseIn)
+          rc.responsePromise.complete(response)
+          pull(responseIn)
+        }
+        override def onPull(): Unit = () // just noting the
+
+        val requestCallback = getAsyncCallback[(HttpRequest, Promise[HttpResponse])] {
+          case (request, responsePromise) =>
+            log.debug(s"Got request $request")
+            if (isAvailable(requestOut)) { // FIXME: is adding the buffer enough for proper handling or will we see unexpected results?
+              log.debug("Dispatching request")
+              val scheme = Uri.httpScheme(hcps.setup.connectionContext.isSecure)
+              val hostHeader = headers.Host(hcps.host, Uri.normalizePort(hcps.port, scheme))
+              val effectiveRequest =
+                request
+                  .withUri(request.uri.toHttpRequestTargetOriginForm)
+                  .withDefaultHeaders(hostHeader)
+              val retries = if (request.method.isIdempotent) hcps.setup.settings.maxRetries else 0
+              push(requestOut, RequestContext(effectiveRequest, responsePromise, retries))
+            } else {
+              responsePromise.tryFailure(PoolOverflowException)
+            }
+        }
+        val shutdownCallback = getAsyncCallback[Promise[Done]] { p =>
+
+        }
+
+        override def request(request: HttpRequest, responsePromise: Promise[HttpResponse]): Unit = requestCallback.invoke(request, responsePromise)
+        override def shutdown()(implicit ec: ExecutionContext): Future[Done] = {
+          val promise = Promise[Done]()
+          shutdownCallback.invoke(promise)
+          promise.future
+        }
+      }
+      (Logic, Logic)
+    }
   }
 
   private final case class PoolRequest(request: HttpRequest, responsePromise: Promise[HttpResponse]) extends NoSerializationVerificationNeeded
@@ -44,7 +157,8 @@ private object PoolInterfaceActor {
 
   private val name = SeqActorName("PoolInterfaceActor")
 
-  private def props(gateway: PoolGateway)(implicit fm: Materializer) = Props(new PoolInterfaceActor(gateway)).withDeploy(Deploy.local)
+  private def props(gateway: PoolGateway)(implicit fm: Materializer) =
+    Props(new PoolInterfaceActor(gateway)).withDeploy(Deploy.local)
 
   /**
    * LogSource for PoolGateway instances
@@ -93,6 +207,7 @@ private class PoolInterfaceActor(gateway: PoolGateway)(implicit fm: Materializer
    * will be redispatched through the gateway. Once all requests were handled this pool shuts down itself.
    */
   private[this] var shuttingDown: Boolean = false
+  var shutdownRequesters: Set[ActorRef] = Set.empty
 
   private[this] val PoolOverflowException = new BufferOverflowException( // stack trace cannot be prevented here because `BufferOverflowException` is final
     s"Exceeded configured max-open-requests value of [${inputBuffer.capacity}]. This means that the request queue of this pool (${gateway.hcps}) " +
@@ -185,6 +300,7 @@ private class PoolInterfaceActor(gateway: PoolGateway)(implicit fm: Materializer
       responsePromise.completeWith(gateway(request))
 
     case Shutdown => // signal coming in from gateway
+      shutdownRequesters += sender
       while (!inputBuffer.isEmpty) {
         val PoolRequest(request, responsePromise) = inputBuffer.dequeue()
         responsePromise.completeWith(gateway(request))
@@ -226,4 +342,6 @@ private class PoolInterfaceActor(gateway: PoolGateway)(implicit fm: Materializer
 
   private def shouldStopOnIdle(): Boolean =
     !shuttingDown && remainingRequested == 0 && hcps.setup.settings.idleTimeout.isFinite && hcps.setup.settings.minConnections == 0
+
+  override def postStop(): Unit = shutdownRequesters.foreach(_ ! Done)
 }
